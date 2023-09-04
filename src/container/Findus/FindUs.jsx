8036e513d562a66a59eb1f64de13d558184dea37
Boyani <sylviaboyani11@gmail.com>
2023-09-04 17:06:30 +0000

    add Contact and footer

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className=" app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>Find Us</h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">Kajiado North, Ngong, Matasia, katana village.</p>
+        <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
+        <p className="p__opensans">Mon - Fri: 8:00 am - 05:00 pm</p>
+        <p className="p__opensans">Sat - Sun: 10:00 am - 01:00 pm</p>
+      </div>
+      <button type="button" className="custom__button" style={{ marginTop: '2rem' }}>Book Us Now!</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="finus_img" />
+    </div>
   </div>
 );
 
